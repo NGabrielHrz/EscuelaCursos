a00c40336048c9d38a8c3c9b82e0714f9b433c65
NGabrielHrz <rotsen.soher@gmail.com>
2023-05-10 16:34:32 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <meta name="description" content="">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <div class="head">
+      <h1>ESCUELA DE CURSOS DE CAPACITACIÓN</h1>
+    </div>
+  </header>
+  <article>
+    <img src="img/fotocampus.png" alt="">
+  </article>
+</body>
+</html>
